5f31e8bfe6482a4c5e170dc4eec995a5c2ac4758
Pravin Raja <33183171+Pravinraja@users.noreply.github.com>
2017-11-25 10:56:27 -0500

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,9 @@
+// this is a test 
+public class FirstProgram{
+   public static void main(String args[]){
+      System.out.println("Java For Noobs");
+      System.out.println("60000*77000 =" + 60000*77000);
+      System.out.println(60000*77000 + "60000*77000 =");
+      }
+
+}
